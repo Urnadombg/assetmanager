fe322ce4428d79870122b9db8ee46e090815682f
Range <urandombg@gmail.com>
2019-09-04 08:22:35 +0300

    LAG

--- database/migrations/2019_09_03_152223_add_email_to_customers.php
+++ database/migrations/2019_09_03_152223_add_email_to_customers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddLegalEntitiesIdToCustomers extends Migration
+class AddEmailToCustomers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddLegalEntitiesIdToCustomers extends Migration
     public function up()
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->integer('legalEntity_id');
+            //
         });
     }
 
